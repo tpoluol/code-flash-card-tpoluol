f11aa02a102deadcf0f35068967b7207394764e6
Yang Hong Seok <davidyang2149@gmail.com>
2022-08-28 00:11:21 +0900

    :hammer: 프로토타입을 제작합니다

---
+++ frontend/code-flash-card/src/pages/EndPage.tsx
@@ -0,0 +1,11 @@
+export default function EndPage() {
+  return (
+    <div>
+      <h3>축하합니다! hashtagName 카드를 모두 학습했어요!</h3>
+      // 인증서 그래픽 이 어떤걸 의미하는거지...?
+      <a>메인으로 이동하기</a>
+      {' '}
+      // link 태그를 사용해도 괜찮겠지,?
+    </div>
+  )
+}
