940971f55156bb53c7355c04f324015229564995
DESKTOP-1E9IN30\82104 <tpoluol3@gmail.com>
2022-08-28 22:24:12 +0900

    css 작업중 병합

--- frontend/code-flash-card/src/components/FlashCard.tsx
+++ frontend/code-flash-card/src/components/FlashCard.tsx
@@ -8,33 +8,26 @@ import { Card } from "./FlashCards";
 export default function FlashCard({ card }: { card: Card }) {
     const { explain, viewCount, cardId } = card;
     return (
-        <Styled.Container>
-            <Styled.CardItem>
-                <Link to={`/detail/${cardId}`}>
-                    <p>{explain}</p>
-                    <span>{viewCount}</span>
-                </Link>
-            </Styled.CardItem>
-        </Styled.Container>
+        <Styled.CardItem>
+            <Link to={`/detail/${cardId}`}>
+                <p>{explain}</p>
+                <span>{viewCount}</span>
+            </Link>
+        </Styled.CardItem>
     );
 }
 
-const Container = styled.ul`
-    display: grid;
-    // grid-template-columns : 1fr 1fr;
-    grid-template-rows: 1fr 1fr;
-    gap: 16px;
-`
-
 const CardItem = styled.li`
-    width: 50%
+    width: 156px;
     height: 156px;
-// left: 16px;
-// top: 122px;
+    background: #36e1c2;
+    box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.25);
+    border-radius: 16px;
+    margin: 0 10px 16px 0;
 
-background: #36E1C2;
-Box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.25);
-// border-radius: 16px;
-`
+    // :nth-of-type(2n) {
+    //     margin-right: 0;
+    // }
+`;
 
-const Styled = { CardItem, Container };
+const Styled = { CardItem };
--- frontend/code-flash-card/src/components/FlashCards.tsx
+++ frontend/code-flash-card/src/components/FlashCards.tsx
@@ -1,35 +1,33 @@
-import React from 'react';
+import React from "react";
 
 import FlashCard from "./FlashCard";
 import styled from "@emotion/styled";
 
 // TODO: UI View단을 위한 interface 보관 장소 만들기
 export interface Card {
-  cardId: number;
-  explain: string;
-  viewCount: number;
+    cardId: number;
+    explain: string;
+    viewCount: number;
 }
 
 export default function FlashCards({ cards }: { cards: Card[] }) {
-  return (
-    <Styled.Cover>
-      {
-        cards.map((card) => {
-          return (
-            <React.Fragment key={card.cardId}>
-              <FlashCard card={card} />
-            </React.Fragment>
-          )
-        })
-      }
-    </Styled.Cover>
-  );
+    return (
+        <Styled.Cover>
+            {cards.map((card) => {
+                return (
+                    <React.Fragment key={card.cardId}>
+                        <FlashCard card={card} />
+                    </React.Fragment>
+                );
+            })}
+        </Styled.Cover>
+    );
 }
 
 const Cover = styled.ul`
-  display: flex;
-  gap: 20px 2%;
-  flex-wrap: wrap;
-`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    // gap: 16px;
+`;
 
 const Styled = { Cover };
